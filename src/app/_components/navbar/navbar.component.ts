fca4f881cfe87421602689970e970de5b28bd4ca
jc-alvarez-fdez <jc.alvarez.fdez@gmail.com>
2024-03-02 21:25:25 +0100

    primeros estilos

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-//import { LoginComponent } from '../account/login/login.component';
 import { MatDialog } from '@angular/material/dialog';
 import { RegisterComponent } from '../account/register/register.component';
 import { ComunicationService } from '../../_services';
@@ -15,40 +14,7 @@ import { AccountService } from '../../_services';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent implements OnDestroy {
+export class NavbarComponent  {
 
-  // private subscription: Subscription;
-  //public userToken = this.AccountService.userToken;
 
-  // public menuItems = routes
-  //   .filter(route => route && route.path)
-  //   .filter(route => route && !route.path?.includes(':'))
-  //   .filter(route => route && !route.path?.includes('**'))
-  //   .filter(route => route && !route.path?.includes('not-found'));
-
-  /* constructor(
-    public dialog: MatDialog,
-    private dialogService: ComunicationService,
-    public AccountService: AccountService
-    ) {
-    this.subscription = this.dialogService.closeDialog.subscribe(() => {
-      this.dialog.closeAll();
-    });
-  }
- */
-/*   openLogIn(): void {
-    this.dialog.open(LoginComponent);
-  } */
-
-/*   openRegister(): void {
-    this.dialog.open(RegisterComponent);
-  }
-
-  logOut(): void {
-    this.AccountService.logOut();
-  }
-*/
-  ngOnDestroy() {
-    //this.subscription.unsubscribe();
-  }
 }
